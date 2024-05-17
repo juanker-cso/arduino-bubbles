ebdf8d7d35007be6ed1ad095ff7ae91405592308
juanker-cso <editors.unboxed475@passinbox.com>
2024-05-17 15:13:42 -0600

    Add files via upload

---
+++ IOT-bubbles/IOT-bubbles.ino
@@ -0,0 +1,60 @@
+/*************************************************************
+  WARNING!
+    It's very tricky to get it working. Please read this article:
+    http://help.blynk.cc/hardware-and-libraries/arduino/esp8266-with-at-firmware
+
+  You’ll need:
+   - Blynk IoT app (download from App Store or Google Play)
+   - Arduino Uno board
+   - Decide how to connect to Blynk
+     (USB, Ethernet, Wi-Fi, Bluetooth, ...)
+
+  There is a bunch of great example sketches included to show you how to get
+  started. Think of them as LEGO bricks  and combine them as you wish.
+  For example, take the Ethernet Shield sketch and combine it with the
+  Servo example, or choose a USB sketch and add a code from SendData
+  example.
+ *************************************************************/
+
+/* Fill-in information from Blynk Device Info here */
+#define BLYNK_TEMPLATE_ID           "TMPL2WvGNcgiu"
+#define BLYNK_TEMPLATE_NAME         "Bubble blower"
+#define BLYNK_AUTH_TOKEN            "n50Rd7CExOKCNMwIOSxjpOsdxEcOPIBa"
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266_Lib.h>
+#include <BlynkSimpleShieldEsp8266.h>
+
+char ssid[] = "MC2G";
+char pass[] = "AJ#38125801";
+
+#include <SoftwareSerial.h>
+SoftwareSerial EspSerial(2, 3); // RX, TX
+
+// Your ESP8266 baud rate:
+#define ESP8266_BAUD 38400
+
+ESP8266 wifi(&EspSerial);
+
+void setup()
+{
+  // Debug console
+  Serial.begin(38400);
+
+  // Set ESP8266 baud rate
+  EspSerial.begin(ESP8266_BAUD);
+  delay(10);
+
+  Blynk.begin(BLYNK_AUTH_TOKEN, wifi, ssid, pass);
+  // You can also specify server:
+  // Blynk.begin(BLYNK_AUTH_TOKEN, wifi, ssid, pass, "blynk.cloud", 80);
+  //Blynk.begin(BLYNK_AUTH_TOKEN, wifi, ssid, pass, IPAddress(192,168,1,100), 8080);
+}
+
+void loop()
+{
+  Blynk.run();
+}
